047061086b9444da0af760175355ddaa47668d03
ArnauXeZ <arnauetxpop@get.cat>
2024-02-22 14:49:00 +0100

    os

---
+++ Assets/Scripts/LevelSizeSelection.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSizeSelection : MonoBehaviour
+{
+    public Slider widthSlider;
+    public Slider heightSlider;
+
+    private int minWidth = 10;
+    private int minHeight = 10;
+    private int maxWidth = 50;
+    private int maxHeight = 50;
+
+    public void StartGame()
+    {
+        int width = (int)widthSlider.value;
+        int height = (int)heightSlider.value;
+
+        // Guardar el tamaño del nivel en PlayerPrefs
+        PlayerPrefs.SetInt("LevelWidth", width);
+        PlayerPrefs.SetInt("LevelHeight", height);
+
+        // Iniciar el juego con el tamaño seleccionado
+        Loader.Load(Loader.Scene.Game);
+    }
+
+    private void Awake()
+    {
+        // Configurar los límites de los deslizadores
+        widthSlider.minValue = minWidth;
+        widthSlider.maxValue = maxWidth;
+        heightSlider.minValue = minHeight;
+        heightSlider.maxValue = maxHeight;
+
+        // Configurar el tamaño inicial de los deslizadores
+        widthSlider.value = minWidth;
+        heightSlider.value = minHeight;
+    }
+}
+
