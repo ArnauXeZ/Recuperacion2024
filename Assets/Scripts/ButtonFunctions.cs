c53331dfff64eb65ecf23be19473479042970527
ArnauXeZ <arnauetxpop@get.cat>
2024-02-23 20:21:09 +0100

    Level 1 ready

---
+++ Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    // Función para cargar la escena del nivel 1
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    // Puedes agregar más funciones para otros botones aquí
+
+}
+
